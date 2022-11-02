8049f2ce61bc8eab2508526de04ad0939bbc5e5e
sileod <damien.sileo@gmail.com>
2022-11-02 15:08:10 +0100

    Delete pygrove directory

--- pygrove/__init__.py
+++
@@ -1,9 +0,0 @@
-from pyforest.utils import get_user_symbols
-
-from .pygrove import *
-
-user_symbols = get_user_symbols()
-pyforest_imports = globals().copy().keys()
-
-for import_symbol in pyforest_imports:
-    user_symbols[import_symbol] = eval(import_symbol)
